0083e5973d4b6da86c80e15437149a7f7ce6493e
adityasuman2025 <adityasuman2025@gmail.com>
2023-11-21 09:36:20 +0530

    admin login screen

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,10 +1,15 @@
 import LoginForm from "mngo-project-tools/comps/LoginForm";
+import { PROJECT_NAME } from "../constants";
 
 export default function Admin() {
     return (
         <div className='mngo-h-screen mngo-flex mngo-items-center mngo-justify-center'>
             <LoginForm
-
+                styles={{
+                    inputClassName: "mngo-light-input mngo-border-none",
+                }}
+                projectTitle={PROJECT_NAME}
+                hideSignUpBtn={true}
             />
         </div>
     )
